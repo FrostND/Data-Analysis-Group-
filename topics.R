055a52a2ef4e89742a2d99d4efd7f298bbd2bad4
Nickolas Frost <nickfrost01@gmail.com>
2023-12-03 13:02:15 -0600

    Create topics.R

---
+++ topics.R
@@ -0,0 +1,13 @@
+# Topics
+# 1. importing and exporting data
+#    1a. quiz
+# 2. data wrangling and manipulation
+#    2a. quiz 
+# 3. basic descriptive statistics 
+#    3a. quiz 
+# 4. basic inferential statistics 
+#    4a. quiz
+# 5. psychometrics 
+#    5a. quiz 
+# 6. statistical modeling 
+#    6a. quiz
